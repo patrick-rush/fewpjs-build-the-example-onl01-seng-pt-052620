d674b347947ddf332a16d98bacebfdcca57dd5fa
patrick-rush <patrick.a.rush@gmail.com>
2020-12-07 20:01:27 -0500

    commit tree 3330a3997e977dcd4b4912d8269900ec9e27c3f2

--- main.js
+++ main.js
@@ -4,8 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let hearts = document.querySelectorAll(".like-glyph")
 
+hearts.forEach(heart => heart.addEventListener("click", e => {
+  mimicServerCall()
+  .then(() => {
+    changeHeart(e.target)    
+    })
+    .catch((error) => {
+      let modal = document.getElementById('modal')
+      modal.classList.remove("hidden")
+      document.getElementById('modal-message').innerText = error
+      setTimeout(() => { addHiddenClass(modal) }, 5000)
+    })
+}))
 
+function addHiddenClass(element) {
+  element.className = "hidden"
+}
+
+function changeHeart(target) {
+  if (target.classList.contains("activated-heart")) {
+    target.classList.remove("activated-heart")
+    target.innerText = EMPTY_HEART
+  } else {
+    target.className = "activated-heart"
+    target.innerText = FULL_HEART
+  }
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
